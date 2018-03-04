6647441a121f36a67103d434ba95166047e3e7cf
mestachs <mestachs>
2018-03-04 18:14:23 +0100

    Extract some methods in Solver

--- lib/orbf/rules_engine/log.rb
+++ lib/orbf/rules_engine/log.rb
@@ -4,7 +4,11 @@ module Orbf
   module RulesEngine
     module Log
       def self.call(string)
-        puts string if ENV['LOG_LEVEL'] == 'debug'
+        puts string if ENV["LOG_LEVEL"] == "debug"
+      end
+
+      def self.error(string)
+        puts "ERROR : #{string}"
       end
     end
   end
--- lib/orbf/rules_engine/services/solver.rb
+++ lib/orbf/rules_engine/services/solver.rb
@@ -4,34 +4,59 @@
 module Orbf
   module RulesEngine
     class Solver
-      attr_reader :variables, :solution
+      attr_reader :variables, :solution, :timings, :equations
 
       def initialize
         @variables = []
+        @timings = {}
       end
 
       def register_variables(vars)
         @variables.push(*vars)
-        duplicates = @variables.group_by(&:key).select { |_key, vals| vals.size > 1 }
-
-        raise "Duplicates for \n#{duplicates.map { |key, vals| [key, '=', "\n\t\t", vals.map(&:expression).join("\n\t\t")].join('') }.join("\n")}" if duplicates.any?
+        duplicates = @variables.group_by(&:key).select { |_, vals| vals.size > 1 }
+        raise duplicate_message(duplicates, vals) if duplicates.any?
       end
 
       def build_problem
-        variables.map { |variable| [variable.key, variable.expression] }.to_h
+        variables.each_with_object({}) do |variable, hash|
+          hash[variable.key] = variable.expression
+        end
       end
 
       def solve!
-        start = Time.now
-        stop = nil
-        problem = build_problem
-        build_time = Time.now - start
+        problem = benchmark("build_time") do
+          build_problem
+        end
+
+        begin
+          benchmark("solve_time") do
+            solve(problem)
+          end
+          RulesEngine::Log.call [
+            "***** problem ",
+            JSON.pretty_generate(problem),
+            "**** solution ",
+            JSON.pretty_generate(solution.map { |k, v| [k, v.to_f] }.to_h),
+            " #{benchmark_log} : problem size=#{problem.size} (#{equations.size})"
+          ].join("\n")
+        rescue StandardError => e
+          RulesEngine::Log.error([
+            "***** problem ",
+            JSON.pretty_generate(problem),
+            "  BUT : #{e.message}"
+          ].join("\n"))
+          raise e
+        end
+        @solution
+      end
+
+      private
+
+      def solve(problem)
         calc = CalculatorFactory.build
         solution = nil
-        equations = {}
+        @equations = {}
         begin
-          # StackProf.run(mode: :cpu, out: "tmp/stackprof-cpu-myapp.dump") do
-
           problem.map do |k, v|
             if v.to_i.to_s == v || v.to_f.to_s == v
               calc.store(k => v.to_f)
@@ -39,26 +64,30 @@ module Orbf
               equations[k] = v
             end
           end
-
           solution = calc.solve!(equations)
-        # end
-        ensure
-          stop = Time.now
         end
-        solution_time = stop - start
-        RulesEngine::Log.call "***** problem #{solution_time}"
-        RulesEngine::Log.call JSON.pretty_generate(problem)
-        RulesEngine::Log.call "**** solution #{Time.now}"
-
-        RulesEngine::Log.call JSON.pretty_generate(solution.map { |k, v| [k, v.to_f] }.to_h)
-
-        RulesEngine::Log.call " solved in #{solution_time}, built in #{build_time} : problem size=#{problem.size} (#{equations.size})"
         @solution = solution
-      rescue StandardError => e
-        RulesEngine::Log.call "***** problem #{stop - start}"
-        RulesEngine::Log.call JSON.pretty_generate(problem)
-        RulesEngine::Log.call "  BUT : #{e.message}"
-        raise e
+      end
+
+      def benchmark(message)
+        start = Time.now
+        value = nil
+        begin
+          value = yield
+        ensure
+          elapsed_time = Time.now - start
+          timings[message] = elapsed_time
+          RulesEngine::Log.call "#{message} #{elapsed_time}"
+        end
+        value
+      end
+
+      def benchmark_log
+        timings.map { |k, timing| [k, timing.to_s].join(" ") }.join(", ")
+      end
+
+      def duplicate_message(duplicates, _vals)
+        "Duplicates for \n#{duplicates.map { |key, vals| [key, '=', "\n\t\t", vals.map(&:expression).join("\n\t\t")].join('') }.join("\n")}"
       end
     end
   end
