2e850cc864fb9b4fe79ed54f61c888389d5db419
Alfred Antoine <aantoine@bluesquarehub.com>
2018-06-07 13:03:07 +0200

    fix according to pronto

--- lib/orbf/rules_engine/builders/package_decision_variables_builder.rb
+++ lib/orbf/rules_engine/builders/package_decision_variables_builder.rb
@@ -31,13 +31,11 @@ module Orbf
       def decision_variables(decision_table)
         orgunits.each_with_object([]) do |orgunit, array|
           input_facts = orgunit.facts
-
           output_facts = decision_table.find(input_facts)
           unless output_facts
             Orbf::RulesEngine::Log.call "WARN : no facts for #{orgunit} #{input_facts} in #{decision_table}"
             next
           end
-
           output_facts.each do |code, value|
             array.push(build_variable(orgunit, code, value))
           end
@@ -47,12 +45,7 @@ module Orbf
       def build_variable(orgunit, code, value)
         Orbf::RulesEngine::Variable.new_package_decision_table(
           period:         period,
-          key:            suffix_for_package(
-            package.code,
-            code,
-            orgunit,
-            period
-          ),
+          key:            suffix_for_package(package.code, code, orgunit, period),
           expression:     value,
           state:          code,
           orgunit_ext_id: orgunit.ext_id,
