f945ad778f0b80e3d846eb02022711ae585d4c81
Stéphan Mestach <smestach@bluesquare.org>
2018-12-12 09:43:28 +0100

    Activity and activity states generated from json

--- spec/sigl_spec.rb
+++ spec/sigl_spec.rb
@@ -5,19 +5,29 @@ RSpec.describe "Sigl System" do
 
   let(:states) do
     raw_cocs["EXosnAykaQv"]["categoryOptionCombos"].map do |coc|
-
       { coc_reference: coc["id"], code: Orbf::RulesEngine::Codifier.codify(coc["name"]), name: coc["name"] }
     end
   end
 
   let(:activities) do
-    raw_activities.map do |activity|
+    raw_activities.map do |raw_activity|
+      name = (raw_activity[1] + " " + raw_activity[2]).gsub("\n"," ").squeeze(' ')
+      code = "mal" + raw_activity[0]
       Orbf::RulesEngine::Activity.with(
-        name:            "mal" + activity[0],
-        activity_code:   activity[1] + " " + activity[2],
-        activity_states: states.map do |_state|
-                           byebug
-                         end
+        activity_code:   code,
+        name:            name,
+        activity_states: states.map do |state|
+                           raw_activity_state = raw_activity.find { |a| a && a["input"] && a["input"]["id"] && a["input"]["id"].include?(state[:coc_reference]) }
+                           next unless raw_activity_state
+                           composites = raw_activity_state["input"]["id"].split("-")
+                           activity_state_name =  (name + " - " + state[:name]).gsub("\n"," ").squeeze(' ')
+                           Orbf::RulesEngine::ActivityState.new_indicator(
+                             state:   state[:code],
+                             ext_id:  "dhis2_#{code}_#{state[:code]}_indicator",
+                             expression: composites[0..1].join("."),
+                             name: activity_state_name
+                           )
+                         end.compact
       )
     end
   end
@@ -28,8 +38,9 @@ RSpec.describe "Sigl System" do
         Orbf::RulesEngine::Package.new(
           code:       :facility,
           kind:       :zone,
-          frequency:  :quarterly,
+          frequency:  :monthly,
           activities: activities,
+          target_org_unit_group_ext_ids: ["dhis_facilities"],
           rules:      [
             Orbf::RulesEngine::Rule.new(
               kind:     :activity,
@@ -43,6 +54,7 @@ RSpec.describe "Sigl System" do
   end
 
   it "works" do
-    activities
+    project
+    puts YAML.dump(project, BestWidth: 500, :use_version => true)
   end
 end
