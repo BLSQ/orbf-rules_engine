77bebe397b161732634d9714d09074503a44314b
pjaspers <pjaspers@bluesquarehub.com>
2019-01-10 10:16:42 +0100

    Move to substitution builder

--- lib/orbf/rules_engine/builders/activity_formula_variables_builder.rb
+++ lib/orbf/rules_engine/builders/activity_formula_variables_builder.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-
+require_relative "./substitution_builder"
 module Orbf
   module RulesEngine
     class ActivityFormulaVariablesBuilder
@@ -17,10 +17,6 @@ module Orbf
       end
 
       private
-
-      EMPTY_SUBSTITUTIONS = {}.freeze
-
-      LEVELS_RANGES = (1..5).freeze
 
       attr_reader :package, :orgunits, :period
 
@@ -87,85 +83,7 @@ module Orbf
       end
 
       def substitutions(formula, activity_code)
-        hashes = [
-          states_substitutions(activity_code),
-          null_substitutions(activity_code),
-          level_substitutions(formula, activity_code),
-          package_substitutions,
-          formulas_substitutions(activity_code),
-          zone_main_orgunit_substitutions(activity_code),
-          decision_table_substitutions(activity_code),
-          orgunit_counts_substitutions(activity_code),
-          dates_substitutions()
-        ]
-        hashes.each_with_object({}) { |hash, acc| acc.merge!(hash) }
-      end
-
-      def dates_substitutions()
-        @period_facts ||= Orbf::RulesEngine::PeriodFacts.for(period)
-      end
-
-      def null_substitutions(activity_code)
-        activity = package.activities.detect { |candidate| candidate.activity_code == activity_code }
-        package.harmonized_activity_states(activity).each_with_object({}) do |activity_state, hash|
-          suffixed_state = suffix_is_null(activity_state.state)
-          hash[suffixed_state] = suffix_activity_pattern(package.code, activity_code, suffixed_state)
-        end
-      end
-
-      def orgunit_counts_substitutions(activity_code)
-        return {} unless package.subcontract?
-
-        counts = Orbf::RulesEngine::ContractVariablesBuilder::COUNTS
-        counts.each_with_object({}) do |count, hash|
-          hash[count] = suffix_activity_pattern(package.code, activity_code, count)
-        end
-      end
-
-      def states_substitutions(activity_code)
-        package.activities.each_with_object({}) do |activity, hash|
-          next if activity_code != activity.activity_code
-
-          package.harmonized_activity_states(activity).each do |activity_state|
-            hash[activity_state.state] = activity_state_substitution(package.code, activity, activity_state)
-          end
-        end
-      end
-
-      def level_substitutions(formula, activity_code)
-        return EMPTY_SUBSTITUTIONS unless formula.expression.include?("_level_")
-
-        package.states.each_with_object({}) do |state, hash|
-          LEVELS_RANGES.each do |level_index|
-            state_level = state + "_level_#{level_index}"
-            hash[state_level] = suffix_activity_pattern(
-              package.code, activity_code, state_level,
-              "orgunit_parent_level#{level_index}_id".to_sym
-            )
-          end
-        end
-      end
-
-      def package_substitutions
-        package.package_rules.flat_map(&:formulas).each_with_object({}) do |formula, hash|
-          hash[formula.code] = suffix_package_pattern(package.code, formula.code)
-        end
-      end
-
-      def formulas_substitutions(activity_code)
-        package.activity_rules.flat_map(&:formulas).each_with_object({}) do |formula, hash|
-          hash[formula.code] = suffix_activity_pattern(package.code, activity_code, formula.code)
-        end
-      end
-
-      def decision_table_substitutions(activity_code)
-        package.activity_rules
-               .flat_map(&:decision_tables)
-               .each_with_object({}) do |decision_table, hash|
-          decision_table.headers(:out).each do |header_out|
-            hash[header_out] = suffix_activity_pattern(package.code, activity_code, header_out)
-          end
-        end
+        SubstitutionBuilder.new(package: package, expression: formula.expression, activity_code: activity_code, period: period).call
       end
 
       def entities_aggregation_values(activity_code)
