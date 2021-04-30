4a2ef1fabb9d1b288eea6dad5854c5216ba169aa
Stéphan Mestach <smestach@bluesquare.org>
2021-04-30 11:11:40 +0200

    Add rpsec for periods on decision tables

--- spec/lib/orbf/rules_engine/builders/decision_variables_builder_spec.rb
+++ spec/lib/orbf/rules_engine/builders/decision_variables_builder_spec.rb
@@ -104,4 +104,126 @@ RSpec.describe Orbf::RulesEngine::DecisionVariablesBuilder do
   it "build" do
     expect(builder.to_variables).to eq_vars expected_variables
   end
+
+  describe "2 decision tables with periods" do
+    let(:package) do
+      Orbf::RulesEngine::Package.new(
+        code:       :facility,
+        kind:       :single,
+        frequency:  :quarterly,
+        activities: activities,
+        rules:      [
+          Orbf::RulesEngine::Rule.new(
+            kind:            :activity,
+            formulas:        [],
+            decision_tables: [
+              Orbf::RulesEngine::DecisionTable.new(%(in:activity_code,in:level_2,out:equity_bonus
+                    act1,county_id,1
+                    act2,county_id,2
+                  ), start_period: "2020Q1", end_period: "2020Q1"),
+              Orbf::RulesEngine::DecisionTable.new(%(in:activity_code,in:level_2,out:equity_bonus
+                  act1,county_id,4
+                  act2,county_id,8
+                ), start_period: "2020Q2", end_period: "2020Q4")
+
+            ]
+          )
+        ]
+      )
+    end
+    let(:builder2020Q1) do
+      Orbf::RulesEngine::DecisionVariablesBuilder.new(
+        package,
+        Orbf::RulesEngine::OrgUnits.new(orgunits: orgunits, package: package),
+        "2020Q1"
+      )
+    end
+
+    let(:expected_variables_2020Q1) do
+      [
+        Orbf::RulesEngine::Variable.with(
+          period:         "2020Q1",
+          key:            "facility_act1_equity_bonus_for_1_and_2020q1",
+          expression:     "1",
+          state:          "equity_bonus",
+          activity_code:  "act1",
+          type:           "activity_rule_decision",
+          orgunit_ext_id: "1",
+          formula:        nil,
+          package:        package,
+          payment_rule:   nil
+        ),
+
+        Orbf::RulesEngine::Variable.with(
+          period:         "2020Q1",
+          key:            "facility_act2_equity_bonus_for_1_and_2020q1",
+          expression:     "2",
+          state:          "equity_bonus",
+          activity_code:  "act2",
+          type:           "activity_rule_decision",
+          orgunit_ext_id: "1",
+          formula:        nil,
+          package:        package,
+          payment_rule:   nil
+        )
+      ]
+    end
+
+    let(:builder2020Q2) do
+      Orbf::RulesEngine::DecisionVariablesBuilder.new(
+        package,
+        Orbf::RulesEngine::OrgUnits.new(orgunits: orgunits, package: package),
+        "2020Q2"
+      )
+    end
+
+    let(:expected_variables_2020Q2) do
+      [
+        Orbf::RulesEngine::Variable.with(
+          period:         "2020Q2",
+          key:            "facility_act1_equity_bonus_for_1_and_2020q2",
+          expression:     "4",
+          state:          "equity_bonus",
+          activity_code:  "act1",
+          type:           "activity_rule_decision",
+          orgunit_ext_id: "1",
+          formula:        nil,
+          package:        package,
+          payment_rule:   nil
+        ),
+
+        Orbf::RulesEngine::Variable.with(
+          period:         "2020Q2",
+          key:            "facility_act2_equity_bonus_for_1_and_2020q2",
+          expression:     "8",
+          state:          "equity_bonus",
+          activity_code:  "act2",
+          type:           "activity_rule_decision",
+          orgunit_ext_id: "1",
+          formula:        nil,
+          package:        package,
+          payment_rule:   nil
+        )
+      ]
+    end
+
+    let(:builder2016Q1) do
+      Orbf::RulesEngine::DecisionVariablesBuilder.new(
+        package,
+        Orbf::RulesEngine::OrgUnits.new(orgunits: orgunits, package: package),
+        "2016Q1"
+      )
+    end
+
+    it "build with first decision table in 2020Q1, first decision table values" do
+      expect(builder2020Q1.to_variables).to eq_vars expected_variables_2020Q1
+    end
+    it "build with first decision table in 2020Q1, second decision table values" do
+      expect(builder2020Q2.to_variables).to eq_vars expected_variables_2020Q2
+    end
+
+    it "build with first decision table in 2016Q1, no variables" do
+      expect(builder2016Q1.to_variables).to eq_vars []
+    end
+  end
 end
